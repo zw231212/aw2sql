d379710e7355821c3ad9e4252c2af09b07c08254
zzq <191550636@qq.com>
2018-09-29 22:14:56 +0800

    bash脚本读取配置文件信息，并且读取DataDir下的全部文件信息

--- aw2sql.sh
+++ aw2sql.sh
@@ -1,2 +1,55 @@
-#!bin/bash
-echo "aw2sql"
+#!/bin/bash
+# *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
+#  > Author: zhangzhiquan
+#  > Mail  : zhangzq_job@163.com 191550636@qq.com
+#  > Gmail : zw231212@gmail.com
+#  > Web   : https://github.com/zw231212/aw2sql
+# *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
+
+echo "aw2sql:=========================================="
+
+#读取配置文件信息
+PREFIX=#
+while read line;do
+  eval "$line"
+done < "conf/aw2sql.conf"
+#打印配置文件的信息
+echo "awstats results data dir: $DataDir"
+echo "=database infomation: "
+echo "host: $dbhost"
+echo "port: $dbport"
+echo "user: $dbuser"
+echo "pass: $dbpass"
+
+#查看文件是否存在
+if [ ! -d $DataDir ];then
+	echo "$DataDir is not existed!"
+	exit 0
+fi
+
+fileInfoArr=[]
+#获取全部的文件,一定要有外面的括号
+files=($(ls $DataDir))
+echo ${#files[@]}
+#=======================
+#文件名格式：awstats122017.share.com.txt
+#也就是awstats+月份+年份+config.txt
+#其中前面的是固定的，也就是月份不足2位的由0补足，然后和最后txt中间的就是config的名称；
+#==========================
+for index in ${!files[@]}; do
+	filename=${files[$index]}
+	#转换为数组
+	info=(${filename//./ })
+	fileInfoArr[$index]=${info[@]}
+done
+
+#echo ${fileInfoArr[45]}
+#ninfo=(${fileInfoArr[45]})
+#echo ${!ninfo[@]}
+echo "文件个数是："${#fileInfoArr[@]}
+fileStorageInfo=[]
+for index in ${!fileInfoArr[@]};do
+	nthInfo=(${fileInfoArr[$index]})
+	echo ${nthInfo[@]}
+done
+
